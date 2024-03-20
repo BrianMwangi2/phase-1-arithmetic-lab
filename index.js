f0082535f1bfa68fd881a428ff94fd0398625a7a
Brian <brian.mwangi2@student.moringaschool.com>
2024-03-20 10:19:11 +0300

    adds calculation

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// multiply
+let num1 =31;
+let num2 =2;
+let multiply = num1 * num2
 
-// Write your code here
+//random
+let random = Math.floor(Math.random() * 10) + 1;
+ let num3 = 9; 
+ let num4 = 5;
+
+//mod
+let mod = num3 % num4; 
+
+//max
+let max = Math.max(2, 5, 6, 10, 20);
+
